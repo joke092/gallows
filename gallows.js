6ae2a788ef0df8ef1c861cacca9be15000df067d
joke092 <porana@vp.pl>
2017-12-24 22:26:32 +0100

    change
    change

--- gallows.js
+++ gallows.js
@@ -1,29 +1,21 @@
-let haslo = "no pain no gain";
+var haslo = "no pain no gain";
 haslo = haslo.toUpperCase();
-
-const dlugosc = haslo.length;
-let ile_skuch = 0;
-
-const yes = new Audio("yes.wav");
-const no = new Audio("no.wav");
-
-let haslo1 = "";
-
-for (i=0; i<dlugosc; i++)
-{
-    if (haslo.charAt(i)==" ") haslo1 = haslo1 + " ";
-    else haslo1 = haslo1 + "-";
+var dlugosc = haslo.length;
+var ile_skuch = 0;
+var yes = new Audio("yes.wav");
+var no = new Audio("no.wav");
+var haslo1 = "";
+for (i = 0; i < dlugosc; i++) {
+    if (haslo.charAt(i) == " ")
+        haslo1 = haslo1 + " ";
+    else
+        haslo1 = haslo1 + "-";
 }
-
-function wypisz_haslo()
-{
+function wypisz_haslo() {
     document.getElementById("plansza").innerHTML = haslo1;
 }
-
 window.onload = start;
-
-const litery = new Array(35);
-
+var litery = new Array(35);
 litery[0] = "A";
 litery[1] = "Ą";
 litery[2] = "B";
@@ -59,80 +51,57 @@ litery[31] = "Y";
 litery[32] = "Z";
 litery[33] = "Ż";
 litery[34] = "Ź";
-
-
-
-function start()
-{
-
-    let tresc_diva ="";
-
-    for (i=0; i<=34; i++)
-    {
+function start() {
+    var tresc_diva = "";
+    for (i = 0; i <= 34; i++) {
         var element = "lit" + i;
-        tresc_diva = tresc_diva + '<div class="litera" onclick="sprawdz('+i+')" id="'+element+'">'+litery[i]+'</div>';
-        if ((i+1) % 7 ==0) tresc_diva = tresc_diva + '<div style="clear:both;"></div>';
+        tresc_diva = tresc_diva + '<div class="litera" onclick="sprawdz(' + i + ')" id="' + element + '">' + litery[i] + '</div>';
+        if ((i + 1) % 7 == 0)
+            tresc_diva = tresc_diva + '<div style="clear:both;"></div>';
     }
-
     document.getElementById("alfabet").innerHTML = tresc_diva;
-
-
     wypisz_haslo();
 }
-
-String.prototype.ustawZnak = function(miejsce, znak)
-{
-    if (miejsce > this.length - 1) return this.toString();
-    else return this.substr(0, miejsce) + znak + this.substr(miejsce+1);
-}
-
-
-function sprawdz(nr)
-{
-
+String.prototype.ustawZnak = function (miejsce, znak) {
+    if (miejsce > this.length - 1)
+        return this.toString();
+    else
+        return this.substr(0, miejsce) + znak + this.substr(miejsce + 1);
+};
+function sprawdz(nr) {
     let trafiona = false;
-
-    for(i=0; i<dlugosc; i++)
-    {
-        if (haslo.charAt(i) == litery[nr])
-        {
-            haslo1 = haslo1.ustawZnak(i,litery[nr]);
+    for (i = 0; i < dlugosc; i++) {
+        if (haslo.charAt(i) == litery[nr]) {
+            haslo1 = haslo1.ustawZnak(i, litery[nr]);
             trafiona = true;
         }
     }
-
-    if(trafiona == true)
-    {
+    if (trafiona == true) {
         yes.play();
         var element = "lit" + nr;
         document.getElementById(element).style.background = "#003300";
         document.getElementById(element).style.color = "#00C000";
         document.getElementById(element).style.border = "3px solid #00C000";
         document.getElementById(element).style.cursor = "default";
-
         wypisz_haslo();
     }
-    else
-    {
+    else {
         no.play();
         var element = "lit" + nr;
         document.getElementById(element).style.background = "#330000";
         document.getElementById(element).style.color = "#C00000";
         document.getElementById(element).style.border = "3px solid #C00000";
         document.getElementById(element).style.cursor = "default";
-        document.getElementById(element).setAttribute("onclick",";");
-
+        document.getElementById(element).setAttribute("onclick", ";");
         //skucha
         ile_skuch++;
-        const obraz = "img/s"+ ile_skuch + ".jpg";
-        document.getElementById("szubienica").innerHTML = '<img src="'+obraz+'" alt="" />';
+        var obraz = "img/s" + ile_skuch + ".jpg";
+        document.getElementById("szubienica").innerHTML = '<img src="' + obraz + '" alt="" />';
     }
-
     //wygrana
     if (haslo == haslo1)
-        document.getElementById("alfabet").innerHTML  = "Tak jest! Podano prawidłowe hasło: "+haslo+'<br /><br /><span class="reset" onclick="location.reload()">JESZCZE RAZ?</span>';
-
+        document.getElementById("alfabet").innerHTML = "Tak jest! Podano prawidłowe hasło: " + haslo + '<br /><br /><span class="reset" onclick="location.reload()">JESZCZE RAZ?</span>';
     //przegrana
-    if (ile_skuch>=9)
-        document.getElementById("alfabet").innerHTML  = "Przegrana! Prawidłowe hasło: "+haslo+'<br /><br /><span class="reset" onclick="location.reload()">JESZCZE RAZ?</span>';
+    if (ile_skuch >= 9)
+        document.getElementById("alfabet").innerHTML = "Przegrana! Prawidłowe hasło: " + haslo + '<br /><br /><span class="reset" onclick="location.reload()">JESZCZE RAZ?</span>';
 }
